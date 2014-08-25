033cbdeb7064b7803f028854d21e49942c3d2a3f
James Morgan <james.morgan.e@googlemail.com>
2014-08-25 09:59:45 +0100

    Basic working angualrjs app

--- js/app.js
+++ js/app.js
@@ -1,3 +1,36 @@
-/**
- * Created by jamesmorgan on 24/08/2014.
- */
+'use strict';
+
+// 3rd Party Dependencies and Libs
+var dependencies = [
+    'ngRoute'
+];
+
+// Application Internal Dependencies
+var appDependencies = [
+    'myApp.controllers',
+    'myApp.services'
+//    'myApp.directives',
+];
+
+angular.module('myApp',
+    appDependencies.concat(dependencies)
+).
+    config(function ($routeProvider, $locationProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'partials/home.html',
+                controller: 'HomeCtrl'
+            }).
+            when('/about', {
+                templateUrl: 'partials/about.html',
+                controller: 'AboutCtrl'
+            }).
+            when('/contact', {
+                templateUrl: 'partials/contact.html',
+                controller: 'ContactCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+//        $locationProvider.html5Mode(true);
+    });
---
+++ js/controllers.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+    controller('HomeCtrl', function ($scope) {
+
+    }).
+    controller('AboutCtrl', function ($scope) {
+
+    }).
+    controller('ContactCtrl', function ($scope) {
+
+    });
